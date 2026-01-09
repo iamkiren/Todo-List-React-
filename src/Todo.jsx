8978b68d83ef51b6e9057ee044ddbd4a5856c231
Kiren Raj <iamkiren@gmail.com>
2026-01-09 12:22:57 +0530

    Basic structure of Todo implemented

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 function Todo() {
-  return <h1>Todo</h1>;
+  const [input, setInput] = React.useState("");
+  const [todoList, setTodoList] = React.useState([
+    { id: 1, text: "Make Tea", completed: true },
+    { id: 2, text: "Morning Walk", completed: false },
+  ]);
+  return (
+    <div>
+      <h1>Todo</h1>
+      <input
+        type="text"
+        placeholder="Enter Todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button>Add</button>
+
+      <ul>
+        {todoList.map((todo) => {
+          return (
+            <li>
+              <input type="checkbox" />
+              <span>{todo.text}</span>
+              <button>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Todo;
